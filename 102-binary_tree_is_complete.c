cad1dd1809affd8102d21f0572f4337e8426bf58
Mohamed Amine <ma.abahmane@gmail.com>
2023-08-03 14:40:15 +0100

    BT

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_complete - a function that checks if a binary tree is complete
+ *
+ * @tree: binary tree to go through
+ *
+ * Return: If tree or func is NULL, do nothing
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+return (1);
+}
